6b6c32662828d3811951e7413ba82726478eeb30
dopamine <crack1933@gmail.com>
2022-03-01 05:33:25 +0700

    Build Fingerprint
    BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys

--- nad_gemini.mk
+++ nad_gemini.mk
@@ -41,9 +41,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.036 7805805 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220205.002 8010174 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys
 
 TARGET_VENDOR := Xiaomi
